5f5dcb25149c2e34ecf9e16f3f3ce78551d796d4
Jozi <koder@creativekode.com>
2020-09-14 21:23:40 -0400

    more code cleanup, plus error handling for invalid query params, importing lodash, not tested tho

--- src/app.ts
+++ src/app.ts
@@ -1,46 +1,46 @@
-import * as express from "express"
-import { json, urlencoded } from "body-parser"
+import * as express from "express";
+import { json, urlencoded } from "body-parser";
 //import * as compression from 'compression';
-import * as cors from "cors"
-import { eventContext } from "aws-serverless-express/middleware"
-import { join } from "path"
-import { getBadgesForAddress } from "./badges"
+import * as cors from "cors";
+import { eventContext } from "aws-serverless-express/middleware";
+import { join } from "path";
+import { getBadgesForAddress } from "./badges";
 // import { checkConsecutiveGovernancePollsCount } from "./badgeActions/governance";
-import { updateRoots } from "./adminActions"
-import discourseMessage from "./utils/discourseMessage"
+import { updateRoots } from "./adminActions";
+import discourseMessage from "./utils/discourseMessage";
 
-export function configureApp () {
-  const app = express()
-  app.set("view engine", "jade")
-  app.use(express.static(join(__dirname, "public")))
+export function configureApp() {
+  const app = express();
+  app.set("view engine", "jade");
+  app.use(express.static(join(__dirname, "public")));
   // app.use(compression());
-  app.use(cors())
-  app.use(json())
-  app.use(urlencoded({ extended: true }))
-  app.use(eventContext())
+  app.use(cors());
+  app.use(json());
+  app.use(urlencoded({ extended: true }));
+  app.use(eventContext());
 
   app.get("/", (req, res) => {
-    res.json({ blah: "test" })
-  })
+    res.json({ blah: "test" });
+  });
 
   app.get("/address/:address", async (req, res) => {
     getBadgesForAddress(req.params.address)
       .then(badgeList => {
-        res.json({ badges: badgeList })
+        res.json({ badges: badgeList });
       })
       .catch(e => {
-        console.log(e)
-      })
-  })
+        console.log(e);
+      });
+  });
 
   app.get("/update-roots", async (req, res) => {
-    updateRoots()
-    res.json({ success: true })
-  })
+    updateRoots();
+    res.json({ success: true });
+  });
 
   app.get("/dev/discourse", (req, res) => {
-    discourseMessage(req, res)
-  })
+    discourseMessage(req, res);
+  });
 
-  return app
+  return app;
 }
--- src/utils/discourseMessage.ts
+++ src/utils/discourseMessage.ts
@@ -1,31 +1,43 @@
-import { getBadgesForAddress } from "../badges"
+import { getBadgesForAddress } from "../badges";
 
-import { ethers } from "ethers"
-import { badgeMap } from "./badgeMap"
-import fetch from "node-fetch"
+import { ethers } from "ethers";
+import { badgeMap } from "./badgeMap";
+import fetch from "node-fetch";
+import _ from "lodash";
 
-const DISCOURSE_BADGES_API: string = process.env.DISCOURSE_BADGES_API!
-const DISCOURSE_API_USERNAME: string = process.env.DISCOURSE_API_USERNAME!
-const DISCOURSE_FORUM_URL: string = process.env.DISCOURSE_FORUM_URL!
+const DISCOURSE_BADGES_API: string = process.env.DISCOURSE_BADGES_API!;
+const DISCOURSE_API_USERNAME: string = process.env.DISCOURSE_API_USERNAME!;
+const DISCOURSE_FORUM_URL: string = process.env.DISCOURSE_FORUM_URL!;
 
-export default function discourseMessage (req, res) {
-  let unlockedBadges: { id: number; description: string }[] = []
-  let discourseBadgeIds: any
-  let errors: any[] = []
-  let responseSent: boolean = false
-  let message = req.query
+const isBlank = value => {
+  return (_.isEmpty(value) && !_.isNumber(value)) || _.isNaN(value);
+};
 
-  let signer = ethers.utils.verifyMessage(message.username, message.signature)
+export default function discourseMessage(req, res) {
+  let unlockedBadges: { id: number; description: string }[] = [];
+  let discourseBadgeIds: any;
+  let errors: any[] = [];
+  let message = req.query;
+
+  if (isBlank(message)) {
+    return res.json({
+      success: false,
+      badgeIds: null,
+      errors: "Invalid request query, please check request params.",
+    });
+  }
+
+  let signer = ethers.utils.verifyMessage(message.username, message.signature);
 
   if (signer.toLowerCase() !== message.address.toLowerCase()) {
-    return res.json({ success: false, errors: "Error signing the message." })
+    return res.json({ success: false, errors: "Error signing the message." });
   }
 
   getBadgesForAddress(signer).then(badgeList => {
     // filter for any unlocked badges
     unlockedBadges = badgeList.filter(badge => {
-      return badge.unlocked === 1
-    })
+      return badge.unlocked === 1;
+    });
 
     // return if no user has no unlocked badges
     if (unlockedBadges.length === 0) {
@@ -33,7 +45,7 @@ export default function discourseMessage (req, res) {
         success: false,
         badgeIds: null,
         errors: "No eligible badges found.",
-      })
+      });
     }
 
     // map over each badge & call discourse badge api
@@ -51,30 +63,30 @@ export default function discourseMessage (req, res) {
             username: message.username,
             badge_id: badgeMap[badge.id],
           }),
-        }
+        };
 
         // fetch discourse API
-        let response = await fetch(`${DISCOURSE_FORUM_URL}`, requestOptions)
+        let response = await fetch(`${DISCOURSE_FORUM_URL}`, requestOptions);
 
         // response error handling
-        if (response.status !== 200 && responseSent === false) {
+        if (response.status !== 200) {
           return res.status(500).json({
             success: false,
             badgeIds: null,
             errors: "Problem connecting to Discourse API.",
-          })
+          });
         }
       }
-    })
+    });
 
     // valid response
     return res.json({
       success: true,
       badgeIds: discourseBadgeIds,
       errors: errors,
-    })
-  })
+    });
+  });
 
   // discourseMessage Failed
-  return false
+  return false;
 }
