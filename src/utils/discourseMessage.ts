4001c7b4ad95456f49866c2c844eea4bb3aad252
Aaron Anderson <aaronlynanderson@gmail.com>
2020-09-17 14:33:11 -0500

    declare fetch method

--- src/utils/discourseMessage.ts
+++ src/utils/discourseMessage.ts
@@ -28,7 +28,7 @@ const discourseMessage = async query => {
     if (!VerifyMessage(query)) { reject(responseObject()); }
 
     // get the unlocked badges from discourse for this user
-    const userAccount = await fetch(`${DISCOURSE_FORUM_URL}?username=${query.username}`);
+    const userAccount = await fetch(`${DISCOURSE_FORUM_URL}?username=${query.username}`, {method:"GET"});
     // console.log("userAccount:", userAccount);
     query.account = await userAccount.json();
     console.log("query.account", query.account);
